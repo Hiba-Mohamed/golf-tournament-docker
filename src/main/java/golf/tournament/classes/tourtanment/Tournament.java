e42c54f0e0fca5311affaf3fe84c079700146dd1
Hiba-Mohamed <hibamoha987@gmail.com>
2025-03-16 00:18:53 -0230

    tournament logic working tested endpointw ith postman all working

--- src/main/java/golf/tournament/classes/tourtanment/Tournament.java
+++ src/main/java/golf/tournament/classes/tourtanment/Tournament.java
@@ -1,4 +1,93 @@
 package golf.tournament.classes.tourtanment;
+import jakarta.persistence.*;
+import golf.tournament.classes.member.Member;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.List;
+
+@Entity
+@Table(name = "tournaments")
 public class Tournament {
+    @Id
+    @SequenceGenerator(name = "tournament_sequence", sequenceName = "tournament_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "tournament_sequence")
+    private long id;
+
+    @DateTimeFormat
+    private String startDate;
+
+    @DateTimeFormat
+    private String endDate;
+
+    private String location;
+
+    private double entryFee;
+
+    private double cashPrizeAmount;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tournament_participants",
+            joinColumns = @JoinColumn(name = "tournament_id"),
+            inverseJoinColumns = @JoinColumn(name = "member_id")
+    )
+    private List<Member> participatingMembers;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getEntryFee() {
+        return entryFee;
+    }
+
+    public void setEntryFee(double entryFee) {
+        this.entryFee = entryFee;
+    }
+
+    public double getCashPrizeAmount() {
+        return cashPrizeAmount;
+    }
+
+    public void setCashPrizeAmount(double cashPrizeAmount) {
+        this.cashPrizeAmount = cashPrizeAmount;
+    }
+
+    public List<Member> getParticipatingMembers() {
+        return participatingMembers;
+    }
+
+    public void setParticipatingMembers(List<Member> participatingMembers) {
+        this.participatingMembers = participatingMembers;
+    }
+
+
 }
